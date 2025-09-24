47a859123723b3898bc3543c81c81213d9e73cdb
Al-Evst <al.evst96@gmail.com>
2025-09-25 00:45:47 +0200

    Create_static + templates

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("catalog/", views.catalog, name="catalog"),
+    path("order/", views.order, name="order"),
+    path("order/step/", views.order_step, name="order_step"),
+    path("consultation/", views.consultation, name="consultation"),
+    path("quiz/", views.quiz, name="quiz"),
+    path("quiz/step/", views.quiz_step, name="quiz_step"),
+    path("result/", views.result, name="result"),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Bouquet
 
-# Create your views here.
+def index(request):
+    recommended = Bouquet.objects.all()[:3]
+    return render(request, "index.html", {"recommended": recommended})
+
+def catalog(request):
+    bouquets = Bouquet.objects.all()
+    return render(request, "catalog.html", {"bouquets": bouquets})
+
+def order(request):
+    return render(request, "order.html")
+
+def order_step(request):
+    return render(request, "order-step.html")
+
+def consultation(request):
+    return render(request, "consultation.html")
+
+def quiz(request):
+    return render(request, "quiz.html")
+
+def quiz_step(request):
+    return render(request, "quiz-step.html")
+
+def result(request):
+    return render(request, "result.html")
