22c3d22f5b8bf341a812e360fcd7dc75350d73e3
Evst404 <al.evst96@gmail.com>
2025-09-25 02:38:02 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import index, quiz, quiz_step, result, catalog, consultation, order, order_step
 
 
-
 urlpatterns = [
     path('', index, name='index'),
     path('quiz/', quiz, name='quiz'),
