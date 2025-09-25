63fd6cb706c9498a879f95af13794f5f70b1cb7a
Al-Evst <al.evst96@gmail.com>
2025-09-25 02:36:25 +0200

    update_templates

--- core/views.py
+++ core/views.py
@@ -1,28 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Bouquet
+
 
 def index(request):
     recommended = Bouquet.objects.all()[:3]
     return render(request, "index.html", {"recommended": recommended})
 
-def catalog(request):
-    bouquets = Bouquet.objects.all()
-    return render(request, "catalog.html", {"bouquets": bouquets})
-
 def order(request):
-    return render(request, "order.html")
+    return render(request, 'order.html')
 
 def order_step(request):
-    return render(request, "order-step.html")
+    return render(request, 'order-step.html')
+
+def catalog(request):
+    bouquets = Bouquet.objects.all()  
+    return render(request, 'catalog.html', {'bouquets': bouquets})
 
 def consultation(request):
-    return render(request, "consultation.html")
+    return render(request, 'consultation.html')
 
 def quiz(request):
-    return render(request, "quiz.html")
+    return render(request, 'quiz.html')
 
 def quiz_step(request):
-    return render(request, "quiz-step.html")
+    return render(request, 'quiz-step.html')
 
-def result(request):
-    return render(request, "result.html")
+def result(request, pk):
+    bouquet = get_object_or_404(Bouquet, pk=pk)  
+    return render(request, 'result.html', {'bouquet': bouquet})
