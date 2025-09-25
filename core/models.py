68e7a68be484a6adafaeb970fb165173cc5a15ef
Katerina <e.mayakovskaya@gmail.com>
2025-09-25 16:46:58 +0300

    Fix choices at models.Bouquet and stylistic edits

--- core/models.py
+++ core/models.py
@@ -5,26 +5,57 @@ from django.core.validators import MinValueValidator
 
 class Bouquet(models.Model):
     OCCASIONS = [
-        ('Свадьба', 'Свадьба'),
-        ('День рождения', 'День рождения'),
-        ('Без повода', 'Без повода'),
-        ('Другой повод', 'Другой повод'),
+        ('wedding', 'Свадьба'),
+        ('birthday', 'День рождения'),
+        ('no_occasion', 'Без повода'),
+        ('other', 'Другой повод'),
     ]
     BUDGETS = [
-        ('до 1000', 'до 1000'),
-        ('1000-5000', '1000-5000'),
-        ('от 5000', 'от 5000'),
-        ('не имеет значения', 'Не имеет значения'),
+        ('up_to_1000', 'До 1000 рублей'),
+        ('1000_5000', '1000-5000 рублей'),
+        ('from_5000', 'От 5000'),
+        ('any', 'Не имеет значения'),
     ]
-    name = models.CharField(verbose_name='Название букета', max_length=50, unique=True)
-    image = models.ImageField(verbose_name='Изображение букета', upload_to='bouquets/', blank=True)
-    price = models.PositiveIntegerField(verbose_name='Цена', default=0, validators=[MinValueValidator(1)])
-    description = models.TextField(verbose_name='Описание', blank=True)
-    composition = models.TextField(verbose_name='Состав', blank=True)
-    occasion = models.CharField(verbose_name='Повод', max_length=50, choices=OCCASIONS, default='Без повода')
-    budget = models.CharField(verbose_name='Бюджет', max_length=50, choices=BUDGETS, default='1000-5000')
+
+    name = models.CharField(
+        verbose_name='Название букета',
+        max_length=50,
+        unique=True
+    )
+    image = models.ImageField(
+        verbose_name='Изображение букета',
+        upload_to='bouquets/',
+        blank=True
+    )
+    price = models.PositiveIntegerField(
+        verbose_name='Цена',
+        default=0,
+        validators=[MinValueValidator(1)]
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True
+    )
+    composition = models.TextField(
+        verbose_name='Состав',
+        blank=True
+    )
+    occasion = models.CharField(
+        verbose_name='Повод',
+        max_length=50,
+        choices=OCCASIONS,
+        default='Без повода'
+    )
+    budget = models.CharField(
+        verbose_name='Бюджет',
+        max_length=50,
+        choices=BUDGETS,
+        default='1000-5000'
+    )
+
     def __str__(self):
         return self.name
+
     class Meta:
         verbose_name = 'Букет'
         verbose_name_plural = 'Букеты'
@@ -34,8 +65,10 @@ class Customer(models.Model):
     first_name = models.CharField('Имя', max_length=50)
     last_name = models.CharField('Фамилия', max_length=50, blank=True)
     phone_number = PhoneNumberField('Номер телефона')
+
     def __str__(self):
         return f'{self.first_name} {self.last_name or ""}'.strip()
+
     class Meta:
         verbose_name = 'Покупатель'
         verbose_name_plural = 'Покупатели'
@@ -44,8 +77,10 @@ class Customer(models.Model):
 class Courier(models.Model):
     name = models.CharField('Имя курьера', max_length=100)
     phone_number = PhoneNumberField('Номер телефона', blank=True)
+
     def __str__(self):
         return self.name
+
     class Meta:
         verbose_name = 'Курьер'
         verbose_name_plural = 'Курьеры'
@@ -56,22 +91,43 @@ class Florist(models.Model):
     florist_id = models.IntegerField(verbose_name='ID флориста', unique=True)
     phone_number = PhoneNumberField(verbose_name='Номер телефона', blank=True)
     is_active = models.BooleanField(verbose_name='Активен', default=True)
+
     def __str__(self):
         return f"{self.name} (ID: {self.florist_id})"
+
     class Meta:
         verbose_name = 'Флорист'
         verbose_name_plural = 'Флористы'
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, verbose_name='Покупатель', on_delete=models.CASCADE)
-    bouquet = models.ForeignKey(Bouquet, verbose_name='Букет', related_name='orders', on_delete=models.CASCADE, null=True, blank=True)
-    courier = models.ForeignKey(Courier, verbose_name='Курьер', on_delete=models.SET_NULL, null=True, blank=True)
+    customer = models.ForeignKey(
+        Customer,
+        verbose_name='Покупатель',
+        on_delete=models.CASCADE
+    )
+    bouquet = models.ForeignKey(
+        Bouquet,
+        verbose_name='Букет',
+        related_name='orders',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    courier = models.ForeignKey(
+        Courier, verbose_name='Курьер',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
     delivery_address = models.CharField(verbose_name='Адрес доставки', max_length=256)
     delivery_time = models.CharField(verbose_name='Время доставки', max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return f'{self.bouquet} для {self.customer}'
+
     class Meta:
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
@@ -82,6 +138,7 @@ class Consultation(models.Model):
     florist = models.ForeignKey(Florist, verbose_name='Флорист', on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     details = models.TextField(verbose_name='Подробности от флориста', blank=True)
+
     def __str__(self):
         return f'Заявка от {self.customer or "Неизвестно"}'
 
