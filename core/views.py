3e717e0dd887ff19c7e8bc9b25de9392242328ca
Evst404 <al.evst96@gmail.com>
2025-09-25 02:37:31 +0200

    Update views.py

--- core/views.py
+++ core/views.py
@@ -6,24 +6,31 @@ def index(request):
     recommended = Bouquet.objects.all()[:3]
     return render(request, "index.html", {"recommended": recommended})
 
+
 def order(request):
     return render(request, 'order.html')
 
+
 def order_step(request):
     return render(request, 'order-step.html')
+
 
 def catalog(request):
     bouquets = Bouquet.objects.all()  
     return render(request, 'catalog.html', {'bouquets': bouquets})
 
+
 def consultation(request):
     return render(request, 'consultation.html')
+
 
 def quiz(request):
     return render(request, 'quiz.html')
 
+
 def quiz_step(request):
     return render(request, 'quiz-step.html')
+
 
 def result(request, pk):
     bouquet = get_object_or_404(Bouquet, pk=pk)  
