11af5c87b5a1c6024bec810cca181a9beebdd94f
Al-Evst <al.evst96@gmail.com>
2025-09-26 15:16:22 +0200

    update_core/templates, admin + create_fixtures_fife

--- core/admin.py
+++ core/admin.py
@@ -1,12 +1,35 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Bouquet, Customer, Courier, Order, Consultation
-
 
 @admin.register(Bouquet)
 class BouquetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'occasion', 'budget')  
-    search_fields = ('name', 'description')  
-    list_filter = ('occasion', 'budget')  
+    list_display = ('id', 'name', 'price', 'occasion', 'budget', 'image_preview')
+    search_fields = ('name', 'description', 'composition')
+    list_filter = ('occasion', 'budget')
+    list_per_page = 10  
+    ordering = ('id',)  
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'price', 'image', 'image_preview'),
+            'description': 'Основные данные букета'
+        }),
+        ('Подробности', {
+            'fields': ('description', 'composition'),
+            'description': 'Описание и состав букета'
+        }),
+        ('Категоризация', {
+            'fields': ('occasion', 'budget'),
+            'description': 'Повод и бюджет для фильтрации'
+        }),
+    )
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 100px; max-width: 100px; object-fit: cover; border-radius: 4px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);" />', obj.image.url)
+        return "Нет изображения"
+    image_preview.short_description = 'Превью'
 
 admin.site.register(Customer)
 admin.site.register(Courier)
